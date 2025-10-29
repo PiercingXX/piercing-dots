86d4f5095d5203c8c2b309789843d9945346602a
Piercingxx <155384428+Piercingxx@users.noreply.github.com>
2025-10-29 09:03:19 -0400

    Delete open-gnome-shortcuts.sh

--- resources/.scripts/Control-Scripts/open-gnome-shortcuts.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Simple script to open gnome-shortcuts.html in the default browser
-
-xdg-open "/media/Working-Storage/GitHub/piercing-dots/resources/.scripts/Control-Scripts/gnome-shortcuts.html"
